ddce83ebd095e5bd6fe764aa341192e6bba8d63c
Rene <racosta323@gmail.com>
2024-03-26 13:05:34 -0500

    solution

--- server/app.py
+++ server/app.py
@@ -5,3 +5,27 @@ from flask_restful import Resource
 
 import ipdb
 
+class Projects(Resource):
+    def get(self):
+        projects = Project.query.all()
+        project_list = [project.to_dict() for project in projects]
+        return make_response(project_list, 200)
+    
+    def post(self):
+        request_body = request.json
+
+        project = Project(
+            name = request_body['name'],
+            about = request_body['about'],
+            phase = request_body['phase'],
+            link = request_body['link'],
+            image = request_body['image']
+        )
+
+        db.session.add(project)
+        db.session.commit()
+
+        return make_response(project.to_dict(), 201)
+
+api.add_resource(Projects, '/projects')
+
